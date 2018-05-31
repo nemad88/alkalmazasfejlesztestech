d52b9a33f1ada337cc345aa4894ddb3101eda44a
Adam Nemeth <nemethadam88@gmail.com>
2018-05-31 22:10:41 +0200

    test case prototype

---
+++ test/szorzas_gifx3q/Szorzas_gifx3qTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package szorzas_gifx3q;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Adam Nemeth
+ */
+public class Szorzas_gifx3qTest {
+    
+    public Szorzas_gifx3qTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Szorzas_gifx3q.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Szorzas_gifx3q.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of multiplyNaturals method, of class Szorzas_gifx3q.
+     */
+    @Test
+    public void testMultiplyNaturals() {
+        System.out.println("multiplyNaturals");
+        int first = 0;
+        int second = 0;
+        int expResult = 0;
+        int result = Szorzas_gifx3q.multiplyNaturals(first, second);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
