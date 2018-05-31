595778fb8b27487e8e88b6047ae085693aa3d83c
Adam Nemeth <nemethadam88@gmail.com>
2018-05-31 22:07:21 +0200

    Üres projekt ;)

---
+++ src/szorzas_gifx3q/Szorzas_gifx3q.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package szorzas_gifx3q;
+
+/**
+ *
+ * @author Adam Nemeth
+ */
+public class Szorzas_gifx3q {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
