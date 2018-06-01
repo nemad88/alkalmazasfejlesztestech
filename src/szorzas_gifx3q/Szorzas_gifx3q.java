2b7f08b80dadb27de00838525350ef9f4aaf3d22
Adam Nemeth <nemethadam88@gmail.com>
2018-06-01 09:11:53 +0200

    about tab added

--- src/szorzas_gifx3q/Szorzas_gifx3q.java
+++ src/szorzas_gifx3q/Szorzas_gifx3q.java
@@ -1,14 +1,20 @@
 package szorzas_gifx3q;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.LayoutManager;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 /**
  * 
@@ -19,20 +25,26 @@ import javax.swing.JSpinner;
 public class Szorzas_gifx3q extends JFrame{
 
     JSpinner firNumberSpinner, secondNumberSpinner;
-    JPanel panel;
+    JPanel szamolPanel;
+    JPanel aboutPanel;
     JButton calculateButton;
-    
-    
+    JTabbedPane tab;
+    JLabel aboutText;
     
     public Szorzas_gifx3q () {
         super("Program");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
-        panel = new JPanel();
-        panel.setLayout(new FlowLayout());
+        szamolPanel = new JPanel();
+        aboutPanel = new JPanel(new BorderLayout());
+        szamolPanel.setLayout(new FlowLayout());
         firNumberSpinner = new JSpinner();
         secondNumberSpinner = new JSpinner();        
         calculateButton = new JButton("Számol");
+        tab = new JTabbedPane();
+        aboutText = new JLabel("<html><center>Németh Ádám<br>nemethadam88@gmail.com</center></html>", SwingConstants.CENTER);
+        
+        
         calculateButton.addActionListener((ae) -> {
             Integer firsttValue = (Integer)firNumberSpinner.getValue();
             Integer secondtValue = (Integer)secondNumberSpinner.getValue();
@@ -40,14 +52,20 @@ public class Szorzas_gifx3q extends JFrame{
             JOptionPane.showMessageDialog(null, "Eredmény: " + firsttValue*secondtValue);
             
         });
+        
         Dimension spinnerDimension = new Dimension(100, 30);
         firNumberSpinner.setPreferredSize(spinnerDimension);
         secondNumberSpinner.setPreferredSize(spinnerDimension);
         
-        panel.add(firNumberSpinner);
-        panel.add(secondNumberSpinner);        
-        panel.add(calculateButton);
-        add(panel);
+        szamolPanel.add(firNumberSpinner);
+        szamolPanel.add(secondNumberSpinner);        
+        szamolPanel.add(calculateButton);
+        aboutPanel.add(aboutText, BorderLayout.CENTER);
+        
+        tab.addTab("Számolás", szamolPanel);
+        tab.addTab("About", aboutPanel);
+        
+        add(tab);
         setSize(300, 300);
         setVisible(true);
     }
